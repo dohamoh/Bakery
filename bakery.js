a6ddfd3848a88745fccf1ba2c3a3718f100a7bb9
dohamoh <doh.mohammed@gmail.com>
2022-09-13 03:07:34 +0300

    sec commit

--- bakery.js
+++ bakery.js
@@ -5,7 +5,7 @@ let navBar = document.getElementById("navBar");
 
 window.onscroll = function () {
     console.log(document.body.scrollTop);
-    if (document.body.scrollTop >= 800) {
+    if (document.body.scrollTop >= 750) {
         navBar.classList.add('nav-colored')
     } else {
         navBar.classList.remove('nav-colored')
